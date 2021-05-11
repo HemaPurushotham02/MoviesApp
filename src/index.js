d2ba26bf6deebd81a52e87b1b5fa6625b9f3af4d
HemaPurushotham02 <hema.purushotham02@gmail.com>
2021-05-11 22:28:44 +0530

    Checkpoint 3

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './common/header/Header.js';
+import Home from './screens/home/Home.js';
 
 ReactDOM.render(
-  <Header/>,
+  <>
+  <Home />
+  </>,
   document.getElementById('root')
 );
 
